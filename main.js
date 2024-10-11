fa5f3ec634d3fef5c81dcf8f3ebf13dff5209fcb
luiz felipe <126171781+luizfelipe153@users.noreply.github.com>
2024-10-11 09:00:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        texto: "Assim que saiu da escola voce se depara com nova tecnologia,um chat que consegue reponder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e audios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso e assustador!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Isso e maravilhoso!"
+                afirmacao: "afirmacao"
+
+            }
+        ]
+
+    }
+]
